22a1bdcb96da4cda012147cc72f627c430bd01f9
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-21 10:07:05 -0300

    feat: create all routes customer

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
+const CustomerController = require('./app/controllers/CustomerController');
 
 const router = Router();
 
-router.get('/', (request, response) => {
-  response.send('Hello');
-});
+router.get('/customer', CustomerController.index);
+router.get('/customer/:id', CustomerController.show);
+router.post('/customer', CustomerController.store);
+router.put('/customer/:id', CustomerController.update);
+router.delete('/customer/:id', CustomerController.delete);
 
 module.exports = router;

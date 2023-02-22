f95ae01d5fa0c7bf2d690fb93f90192b8a5e2e48
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-21 21:08:15 -0300

    feat: add express-async-errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+require('express-async-errors');
 
 const app = express();
 
 const routes = require('./routes');
 
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => {
   console.log('Server is running.');

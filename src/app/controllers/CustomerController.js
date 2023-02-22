184593534fc5357e732d1f7d561d56465564e298
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-21 21:10:43 -0300

    feat: create show in CustomerController

--- src/app/controllers/CustomerController.js
+++ src/app/controllers/CustomerController.js
@@ -5,8 +5,15 @@ class CustomerController {
     response.json(customers);
   }
 
-  show(request, response) {
-    response.send('legal show');
+  async show(request, response) {
+    const { id } = request.params;
+    const customer = await CustomerRepository.findById(id);
+
+    if (!customer) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(customer);
   }
 
   store(request, response) {

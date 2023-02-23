b4bbccf55aba8bd7f2fe1f16d47aefdc4888fcba
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-22 22:36:00 -0300

    feat: add index wallet in routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const CustomerController = require('./app/controllers/CustomerController');
+const WalletController = require('./app/controllers/WalletController');
 
 const router = Router();
 
@@ -8,5 +9,7 @@ router.get('/customer/:id', CustomerController.show);
 router.post('/customer', CustomerController.store);
 router.put('/customer/:id', CustomerController.update);
 router.delete('/customer/:id', CustomerController.delete);
+
+router.get('/wallet', WalletController.index);
 
 module.exports = router;
